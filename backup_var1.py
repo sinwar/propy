6e686099f34ed5773c159817dba0c00dc71198fc
sinwar <premsinwar4@gmail.com>
2016-02-23 01:57:27 +0530

    pep8 issues

--- backup_var1.py
+++ backup_var1.py
@@ -1,12 +1,12 @@
 import os
 import time
 # source from backup should be taken
-source='C://bup'
+source = 'C://bup'
 # target where backup should be inserted
-target_dir=["C://target"]
+target_dir = ["C://target"]
 
 # create target path where should backup files goes in the date time format
-target=target_dir[0]+os.sep+time.strftime("%Y%m%d%H%M%S")+'.zip'
+target = target_dir[0] + os.sep + time.strftime("%Y%m%d%H%M%S") + '.zip'
 
 # create a target directory if it is no there mkdir for make irectory and rmdir for remoobe directory
 if not os.path.exists(target_dir[0]):
